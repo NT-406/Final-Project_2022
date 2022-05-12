f9af1436fb32ff419b25bf1cac5bb97b975df17e
nnphuongtruc23 <95388048+nnphuongtruc23@users.noreply.github.com>
2022-05-12 16:37:09 +0700

    Update milktea.py

--- milktea.py
+++ milktea.py
@@ -2,58 +2,51 @@ from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
 
+class Window:
+    def __init__(self, root):
+        self.root = root
+        self.root.title('Tiệm trà sữa NT')
+        self.root.geometry("925x500+300+150")
+        self.root.configure(bg="#fff")
+        self.root.resizable(False, False)
+
+        self.img = PhotoImage(file=r"C:\Users\USER\Downloads\boba.png")
+        Label(self.root, image=self.img, bg='white', width=300).place(x=60, y=70)
+
+        Label(self.root, text='Trà sữa NT', fg='#FE83C6', bg='white', font=('Arial', 26, 'bold')).place(x=120, y=20)
+
+        self.frame = Frame(self.root, width=350, height=350, bg="white")
+        self.frame.place(x=480, y=70)
+
+        self.heading = Label(self.frame, text='Đăng nhập', fg='#FF5C8D', bg='white', font=('Arial', 23, 'bold'))
+        self.heading.place(x=100, y=5)
+
+        self.user = Entry(self.frame, width=25, fg='black', border=0, bg='white', font=('Arial', 11))
+        self.user.place(x=30, y=80)
+        self.user.insert(0, 'Tên đăng nhập')
+        Frame(self.frame, width=295, height=2, bg='black').place(x=25, y=107)
+        self.user.bind('<FocusIn>', self.on_enter)
+        self.user.bind('<FocusOut>', self.on_leave)
+
+        self.p = Entry(self.frame, width=25, fg='black', border=0, bg='white', font=('Arial', 11))
+        self.p.place(x=30, y=150)
+        Frame(self.frame, width=295, height=2, bg='black').place(x=25, y=177)
+
+        Button(self.frame, width=39, pady=7, text='Đăng nhập', bg='#FE83C6', fg='white', border=0, font=('Arial', 9)).place(x=35, y=204)
+        Label(self.frame, text='Bạn chưa có tài khoản?', fg='black', bg='white', font=('Arial', 9)).place(x=75, y=270)
+
+        self.sign_up = Button(self.frame, width=6, text='Đăng ký', border=0, bg='white', cursor='hand2', fg='#FF5C8D', font=('Arial', 9))
+        self.sign_up.place(x=215, y=270)
+
+    def on_enter(self, event):
+        self.user.delete(0, 'end')
+    
+    def on_leave(self, event):
+        name = self.user.get()
+        if name == '':
+            self.user.insert(0, 'Tên đăng nhập')
+            
+        
 root = Tk()
-root.title('Tiệm trà sữa NT')
-root.geometry("925x500+300+150")
-root.configure(bg="#fff")
-root.resizable(False, False)
-
-img = PhotoImage(file=r"C:\Users\USER\Downloads\boba.png")
-Label(root, image=img, bg='white', width=300).place(x=60, y=70)
-
-Label(root, text='Trà sữa NT', fg='#FE83C6', bg='white', font=('Arial', 26, 'bold')).place(x=120, y=20)
-
-frame = Frame(root, width=350, height=350, bg="white")
-frame.place(x=480, y=70)
-
-heading = Label(frame, text='Đăng nhập', fg='#FF5C8D', bg='white', font=('Arial', 23, 'bold'))
-heading.place(x=100, y=5)
-
-def on_enter(e):
-    user.delete(0, 'end')
-
-def on_leave(e):
-    name = user.get()
-    if name=='':
-        user.insert(0, 'Tên đăng nhập')
-
-
-
-# Tên đăng nhập
-user = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Arial', 11))
-user.place(x=30, y=80)
-user.insert(0, 'Tên đăng nhập')
-Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
-user.bind('<FocusIn>', on_enter)
-user.bind('<FocusOut>', on_leave)
-
-def sign_in():
-    username = user.get()
-    password = p.get()
-
-
-
-# Mật khẩu
-p = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Arial', 11))
-p.place(x=30, y=150)
-Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
-
-#
-Button(frame, width=39, pady=7, text='Đăng nhập', bg='#FE83C6', fg='white', border=0, font=('Arial', 9)).place(x=35, y=204)
-Label(frame, text='Bạn chưa có tài khoản?', fg='black', bg='white', font=('Arial', 9)).place(x=75, y=270)
-
-sign_up = Button(frame, width=6, text='Đăng ký', border=0, bg='white', cursor='hand2', fg='#FF5C8D', font=('Arial', 9))
-sign_up.place(x=215, y=270)
-
-
+t = Window(root)
 root.mainloop()
